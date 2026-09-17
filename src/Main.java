4bae894a2c932742527982522bca5c4b6735968d
Vortiguant <anair0298@gmail.com>
2026-09-17 14:28:55 -0400

    Add sales tax calculation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+class main
 {
     void main() // Public static void main (String[] Args)
     {
+        double purchasePrice = 23.50;
+        double total = 0;
+        double salesTax = 0;
+        final double RATE = 0.05;
+
+        salesTax = RATE * purchasePrice;
+        total = salesTax + purchasePrice;
+
+        IO.println("The total is " + total + " and the tax is " + salesTax);
 
     }
-
 }
